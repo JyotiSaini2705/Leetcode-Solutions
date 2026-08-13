484cec4bd32661bfe3b3cc4a5fea9af4a6050ef2
JyotiSaini <jyotisaini3841@gmail.COM>
2026-08-13 22:04:44 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (93.09%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int j = s.size()-1;
+        
+        while (j >= 0 && s[j] == ' ') {
+            j--;
+        }
+
+        int count = 0;
+
+       
+        while (j >= 0 && s[j] != ' ') {
+            count++;
+            j--;
+        }
+
+        return count;
+    }
+};
